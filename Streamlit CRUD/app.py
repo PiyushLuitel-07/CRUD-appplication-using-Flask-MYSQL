65c8819e68f872b3cfcd5389ea1796c0c469ee92
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-27 07:16:27 +0545

    functionality added!

--- Streamlit CRUD/app.py
+++ Streamlit CRUD/app.py
@@ -9,3 +9,27 @@ mydb=mysql.connector.connect(
     password="9803474343",
     database="crud_new1"
 )
+
+mycursor=mydb.cursor()
+
+#Create Streamlit Application
+
+def main():
+    st.title("Welcome to CRUD Application")
+    # display options for CRUD operation
+    option=st.sidebar.selectbox("Select an operation",("Create","Read","Update","Delete"))
+    # Perform operation
+    if option=="Create":
+        st.subheader("Create new Record")
+        name=st.text_input("Enter your name")
+        email=st.text_input("Enter your E-mail")
+    elif option=="Read":
+        st.subheader("Read Record")
+    elif option=="Update":
+        st.subheader("Update Record")
+    elif option=="Delete":
+        st.subheader("Delete Record")
+
+
+if __name__=="__main__":
+    main()
--- Streamlit CRUD/crud_new1.sql
+++ Streamlit CRUD/crud_new1.sql
@@ -1,2 +1,7 @@
 create database crud_new1;
 use crud_new1;
+create table users(
+id integer primary key auto_increment,
+name varchar(50),
+email varchar(50)
+)
