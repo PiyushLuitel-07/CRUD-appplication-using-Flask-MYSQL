c27a1d065a94ac568b0752e3a2a989b57ca196c1
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-27 07:01:44 +0545

    established the connection

--- Streamlit CRUD/app.py
+++ Streamlit CRUD/app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 import mysql.connector
+
+# Establish a connection to MySQL Server
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9803474343",
+    database="crud_new1"
+)
